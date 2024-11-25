e9575163def8a89396dba9b2c2f479a185fd7284
KaukabFarrukh <144961941+KaukabFarrukh@users.noreply.github.com>
2024-11-25 14:07:56 -0800

    created network manager.swift and used dog facts

--- DogsFactsApp/ContentView.swift
+++ DogsFactsApp/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var networkManager = NetworkManager()
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Random Dog Fact")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            Text(networkManager.dogFact)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Button(action: {
+                networkManager.fetchDogFact()
+            }) {
+                Text("Get Another Fact")
+                    .font(.title3)
+                    .foregroundColor(Color.blue)
+                    .cornerRadius(10)
+            }
         }
         .padding()
+        .onAppear {
+            networkManager.fetchDogFact()
+        }
     }
+
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- DogsFactsApp/NetworkManager.swift
+++ DogsFactsApp/NetworkManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class NetworkManager: ObservableObject {
+    @Published var dogFact: String = ""
+    
+    private let urlString = "https://dog-api.kinduff.com/api/facts"
+    
+    func fetchDogFact() {
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching dog fact: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(DogFactResponse.self,  from: data)
+                DispatchQueue.main.async {
+                    self.dogFact = decodedData.facts.first ?? "No fact available"
+                }
+            } catch {
+                print("Error decoding data: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
+
+struct DogFactResponse: Codable {
+    let facts: [String]
+}
