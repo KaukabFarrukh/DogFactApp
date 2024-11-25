0d81c35c92963f241c03f4d55364102df5a43179
KaukabFarrukh <144961941+KaukabFarrukh@users.noreply.github.com>
2024-11-25 13:17:10 -0800

    Initial Commit

---
+++ DogsFactsApp/DogsFactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsFactsAppApp.swift
+//  DogsFactsApp
+//
+//  Created by Kaukab Farrukh on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct DogsFactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogsFactsAppTests/DogsFactsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogsFactsAppTests.swift
+//  DogsFactsAppTests
+//
+//  Created by Kaukab Farrukh on 2024-11-25.
+//
+
+import Testing
+@testable import DogsFactsApp
+
+struct DogsFactsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
